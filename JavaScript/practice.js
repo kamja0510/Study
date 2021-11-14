d4bd137cb7cbb2d6b11595abf9c91d6332ee63b1
김재민 <kamja990510@gmail.com>
2021-11-14 20:47:49 +0900

    11/14

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -3,9 +3,13 @@
 const myName = "김재민";
 const greeting = "hello " + myName;
 const bye = `Goodbye ${myName}`;
+const num1 = '123';
 console.log(greeting);
 console.log(bye);
-console.log(typeof(null));  // 왜 null값은 object 타입이지?
+console.log(typeof(null));  // A. 왜 null값은 object 타입이지? Q. 버그랍니다.
+console.log(parseInt(num1));
+
+
 
 switch(myName){         // switch 쓰는법
     case "김재민":
@@ -20,12 +24,17 @@ switch(myName){         // switch 쓰는법
         break;
 }
 
+
+
 function printAll(...args){         // rest parameter
     for(const arg of args){         // for of
         console.log(arg);
     }
+    // return undefined;            // 항상 이 문장이 숨겨져 있다고 보면 됀다.
 }
 printAll('신', "기", "하", "다");
+
+
 
 const add = (a,b) => a+b;                      // arrow function
 const multiply = (a,b) => {                    // arrow function, 블럭을 쓸 때는 return을 써줘야 한다.
@@ -35,9 +44,13 @@ const multiply = (a,b) => {                    // arrow function, 블럭을 쓸 
 console.log(add(1,2));
 multiply(2,4);
 
+
+
 (function print(){                  // IIFE
     console.log("IIFE");
 })();
+
+
 
 class User{
     constructor(firstName, lastName){
@@ -59,14 +72,20 @@ console.log(user1.age);
 user1.age = -3;
 console.log(user1.age);
 user1.age = 10;
-console.log(user1._age);            // 실제값은 별도의 property _age에 저장이 된다. 하지만 객체 외부에서 건드리지 않는 것이 관습.
+console.log(user1._age);            // 실제값은 별도의 property _age에 저장이 된다. 하지만 객체 외부에서 건드리지 않는 것이 관습.\
+
+
 
 console.log(user1 instanceof Object);       // 모든 객체들은 Object라는 클래스를 상속받는다.
+
+
 
 function printValue(obj, key){
     console.log(obj[key]);              // computed property. 동적으로 key의 value를 받아올때 유용하게 사용한다.
 }
 printValue(user1, 'firstName');         // key should be always string
+
+
 
 function makePerson(name, age){         // property value shorthand
     return {
@@ -77,6 +96,8 @@ function makePerson(name, age){         // property value shorthand
 const user2 = makePerson('김동진', 24);
 console.log(user2);
 
+
+
 function Person(name, age){             // constructor function
     this.name = name;
     this.age = age;
@@ -84,15 +105,23 @@ function Person(name, age){             // constructor function
 const user3 = new Person('최진철', 23);         // constructor function은 클래스 쓰는 것처럼 쓴다.
 console.log(user3);                     // 일반 object들과 출력이 다르다..
 
+
+
 console.log('name' in user3);           // in operator
+
+
 
 const people = new Array();
 const language = ['python', 'JS', 'Java'];
 console.log(language[language.length - 1]);
 
-language.forEach(function(value, index, array){         // arrow function으로도 가능.
+
+
+language.forEach(function(value, index, array){         // 배열에서의 forEach
     console.log(value, index, array);
 });
+
+
 
 function fetchUser(){                                   // Promise producer
     return new Promise((resolve,reject) => {
@@ -102,6 +131,8 @@ function fetchUser(){                                   // Promise producer
 }
 const practicePromise = fetchUser();
 console.log(practicePromise);
+
+
 
 async function asyncFetchUser(){    
     // do network request in 10 secs...                // async 사용
